34ddf073ba8f49496f07e5eb67e40dff2334c904
Rens Oliemans <hallo@rensoliemans.nl>
2022-01-08 22:20:53 +0100

    :sparkles: Add all categories

--- cities.py
+++ cities.py
@@ -1,5 +1,45 @@
-PLATINUM = [
-    ("oreburgh", "Oreburgh"),
-    ("veilstone", "Veilstone"),
-    ("fight_resort", "Fight/Resort"),
-]
+PLATINUM = {
+    'poi': [
+        ('places_of_interest', 'Places of Interest'),
+        ('pokecenters', 'Pokécenters'),
+    ],
+    'cities': [
+        ('sandgem_jubilife', 'Sandgem/Jubilife'),
+        ('oreburgh', 'Oreburgh'),
+        ('floaroma', 'Floaroma'),
+        ('eterna', 'Eterna'),
+        ('hearthome', 'Hearthome'),
+        ('solaceon', 'Solaceon'),
+        ('veilstone', 'Veilstone'),
+        ('pastoria', 'Pastoria'),
+        ('celestic', 'Celestic'),
+        ('canalave', 'Canalave'),
+        ('snowpoint', 'Snowpoint'),
+        ('sunyshore', 'Sunyshore'),
+        ('fight_resort', 'Fight/Resort'),
+        ('survival', 'Survival Area'),
+    ],
+    'hubs': [
+        ('jubilife_tv', 'Jubilife TV'),
+        ('store', 'Department Store'),
+        ('galactic', 'Galactic'),
+        ('old_chateau_corridor', 'Old Chateau corridor'),
+        ('old_chateau_main', 'Old Chateau Main'),
+        ('solaceon_ruins', 'Solaceon Ruins'),
+        ('pokemansion', 'Pokémon Mansion'),
+    ],
+    'routes': [
+        ('route_208', 'Route 208'),
+        ('eterna_forest', 'Eterna Forest'),
+        ('route_213_214_222', 'R213, R214, R222'),
+        ('great_marsh', 'Great Marsh'),
+    ],
+    'other': [
+        ('mt_coronet', 'Mt Coronet'),
+        ('victory_road', 'Victory Road'),
+        ('iron_island', 'Iron Island'),
+        ('poketch_gts', 'Poketch + GTS'),
+        ('oreburgh_gate', 'Oreburgh Gate'),
+        ('surf_hub', 'Surf Hub'),
+    ]
+}
