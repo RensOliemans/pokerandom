c4a2c01b0630124847bc5c86b1a50f1d7e11246c
Rens Oliemans <hallo@rensoliemans.nl>
2022-01-12 15:53:14 +0100

    :pencil2: Add first help info bits

--- widgets/help.py
+++ widgets/help.py
@@ -1,4 +1,7 @@
-from PySide6.QtWidgets import QWidget, QDialog, QTabWidget, QVBoxLayout
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QWidget, QDialog, QTabWidget, QVBoxLayout, QTextEdit, QLabel
+
+HOTKEY = "Hotkey settings"
 
 
 class Help(QDialog):
@@ -7,7 +10,7 @@ class Help(QDialog):
 
         tab_widget = QTabWidget()
         tab_widget.addTab(HelpTab(self), "Help")
-        tab_widget.addTab(HotkeyTab(self), "Hotkey settings")
+        tab_widget.addTab(HotkeyTab(self), HOTKEY)
 
         main_layout = QVBoxLayout()
         main_layout.addWidget(tab_widget)
@@ -15,9 +18,34 @@ class Help(QDialog):
         self.setWindowTitle("Help")
 
 
-class HelpTab(QWidget):
+class HelpTab(QLabel):
     def __init__(self, parent):
         super().__init__(parent)
+
+        self.setTextInteractionFlags(Qt.TextBrowserInteraction)
+        self.setOpenExternalLinks(True)
+        self.setTextFormat(Qt.RichText)
+
+        self.setText(f"""
+        Click on an entrance and destination to link the two. <br>
+        Press 'q' or Esc to cancel the link, see {HOTKEY} for more hotkeys.
+        <br>
+        Hold Shift while hovering over an entrance to see all desinations available
+        <br>
+        <i>within 2 warps</i>.
+        <br>
+        Control-click on an entrance to immediately switch to its destination.
+                
+        <h3>About</h3>
+        A tool to keep track of the random locations in randomized Pokémon.
+        <br>
+        Credits to the randomization to <a href="https://www.youtube.com/c/PointCrow">PointCrow</a>.
+        <br>
+        See <a href="https://www.reddit.com/r/pokemon/comments/qel5h4/">here</a> for the Reddit
+        post explaining how it works (for Emerald).
+        <br>
+        This tool is open-source, <a href="https://gitlab.com/RensOliemans/pokerandom">here</a> is the source-code.
+        """)
 
 
 class HotkeyTab(QWidget):
